3731a46ed297b3415d85db51204866d5dd56e0d6
zszfympxdebug <denys2010@163.com>
2023-08-01 10:32:08 +0800

    增加剪玩家

--- src/main/java/com/zszf/spawnegg/event/ToEgg.java
+++ src/main/java/com/zszf/spawnegg/event/ToEgg.java
@@ -1,6 +1,7 @@
 package com.zszf.spawnegg.event;
 
 import org.bukkit.Material;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,19 +16,24 @@ public class ToEgg implements Listener {
             e.setCancelled(true);
             EntityType et = e.getRightClicked().getType();
             Material egg;
-            try {
-                String egg_name = (et.name() + "_SPAWN_EGG").toUpperCase();
-                egg = Material.getMaterial(egg_name);
-            } catch (Exception ex) {
-                e.getPlayer().sendMessage("未成功获取生物蛋");
-                return;
-            }
-            if (egg != null) {
-                e.getRightClicked().getWorld().dropItem(e.getRightClicked().getLocation(), new ItemStack(egg));
+            if(e.getRightClicked().getType()!= EntityType.PLAYER){
+                try {
+                    String egg_name = (et.name() + "_SPAWN_EGG").toUpperCase();
+                    egg = Material.getMaterial(egg_name);
+                } catch (Exception ex) {
+                    e.getPlayer().sendMessage("未成功获取生物蛋");
+                    return;
+                }
+                if (egg != null) {
+                    e.getRightClicked().getWorld().dropItem(e.getRightClicked().getLocation(), new ItemStack(egg));
+                    e.getRightClicked().remove();
+                    e.getPlayer().sendMessage("成功将生物蛋割了下来");
+                } else {
+                    e.getPlayer().sendMessage("未成功获取生物蛋");
+                }
+            }else{
                 e.getRightClicked().remove();
-                e.getPlayer().sendMessage("成功将生物蛋割了下来");
-            } else {
-                e.getPlayer().sendMessage("未成功获取生物蛋");
+                e.getRightClicked().getWorld().dropItem(e.getRightClicked().getLocation(), new ItemStack(Material.PIG_SPAWN_EGG));
             }
         }
     }
